fb825d7e1bb900cec9fc7bbd184d4c3c9dfc30cb
Gabriel Victor <gabrielvictorweb@outlook.com>
2024-06-12 20:59:01 -0300

    feat: Header

---
+++ web/src/components/organisms/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+export const Header: React.FC = () => (
+  <S.Container>
+    <S.Logo>To do List</S.Logo>
+  </S.Container>
+);
---
+++ web/src/components/organisms/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 1rem 0;
+  width: 90%;
+  margin: 0 auto;
+  max-width: 1336px;
+`;
+
+export const Logo = styled.h1`
+  font-size: 2rem;
+`;
